60d1f4fc46621ff3736c97058bf3ce7497e6582d
yosephdev <yosephbet@gmail.com>
2024-03-28 05:58:15 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".box").on("click", function() {    	
+    	var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
+});
