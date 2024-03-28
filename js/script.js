e5e2b93c7329bfd49742a11d264fa53ba7491dfb
yosephdev <yosephbet@gmail.com>
2024-03-28 07:19:05 +0000

    Implement dynamic highlighting of module cards on navigation click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-    $(".box").on("click", function() {    	
-    	var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
-    });
+$('.module-nav').on("click", function(){
+    let allModulesCardsSelector = ".card";
+    let thisModuleCardsSelector = "." + this.id + "-card";
+    
+    $(allModulesCardsSelector).removeClass("card-highlight");
+    $(thisModuleCardsSelector).addClass("card-highlight");
 });
+
